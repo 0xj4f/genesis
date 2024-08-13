8a6a6d7d2087d59e03520135050568bd4a39e5b6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2024-08-13 15:46:51 +0800

    Remove comments, legacy code snippets

--- database_config.py
+++ database_config.py
@@ -1,32 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# import os
-#
-# DATABASE_USER = os.getenv("MYSQL_DEV_USER", "root")
-# DATABASE_PASSWORD = os.getenv("MYSQL_DEV_PASSWORD", "")
-# DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-# DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
-# DATABASE_NAME = os.getenv("DATABASE_NAME", "genesis")
-#
-# # Construct the database URL
-# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-#
-# # Create the engine and session
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# Base = declarative_base()
-#
-#
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# Base = declarative_base()
-# database_config.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -38,7 +9,7 @@ DATABASE_PASSWORD = os.getenv("MYSQL_DEV_PASSWORD", "")
 DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
 DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "genesis")
-encoded_password = quote_plus(DATABASE_PASSWORD)
+encoded_password = quote_plus(DATABASE_PASSWORD)  # if password has special characters
 
 # Construct the database URL
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{encoded_password}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
