92201a0c7652de4651ff63cd65ade0016f838d19
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2024-09-03 13:30:03 +0800

    add CORS for development

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.declarative import declarative_base
 from app.routes.users import router as user_router
 from app.routes.profiles import router as profile_router
@@ -7,6 +8,19 @@ from app.database.session import Base,engine
 import logging
 
 app = FastAPI(title="Genesis - Identity Access Management")
+
+# origins = ["http://localhost:8080", "http://127.0.0.1:8000"]
+# ONLY FOR TEST PUPRPOSES
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # or specify ["GET", "POST", ...]
+    allow_headers=["*"],  # or specify ["Content-Type", "Authorization", ...]
+)
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
